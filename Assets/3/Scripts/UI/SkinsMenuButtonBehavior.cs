5d7171943f50d80af3a25f2b2c844395594163da
Shany <doidexx000@gmail.com>
2020-04-15 04:30:22 -0400

    Retouched skin screen elements
    buttons animations and images reworked.
    
    TODO
    tune up animations and add a counter before starting spawning balls.

--- Assets/3/Scripts/UI/SkinsMenuButtonBehavior.cs
+++ Assets/3/Scripts/UI/SkinsMenuButtonBehavior.cs
@@ -13,15 +13,31 @@ namespace ID.UI
         [Header("Skin Material")]
         [SerializeField] Material skin;
 
+        [Header("Image")]
+        [SerializeField] RectTransform icon;
+        [SerializeField] Vector2 iconSizeOnClick = new Vector2(1.5f, 1.5f);
+
         public void OnClick()
         {
             FindObjectOfType<SkinSelectButton>().CheckForSelection(skin);
             ChangePreviewSkin();
+            var otherButtons = FindObjectsOfType<SkinsMenuButtonBehavior>();
+            foreach (SkinsMenuButtonBehavior button in otherButtons)
+                button.ReSize(false); 
+            ReSize(true);
         }
 
         void ChangePreviewSkin()
         {
             characterPreview.material = skin;
+        }
+
+        void ReSize(bool resize)
+        {
+            if (resize)
+                icon.localScale = iconSizeOnClick;
+            else
+                icon.localScale = Vector2.one;
         }
     }
 }
